d49afa7242e07cd79de9d989690765f98ac849ea
Enno Rehling <enno.rehling@gmail.com>
2018-07-22 11:57:16 +0200

    add editorconfig

--- crpat.h
+++ crpat.h
@@ -3,5 +3,18 @@
 struct CR_ParserStruct;
 typedef struct CR_ParserStruct *CR_Parser;
 
+enum CR_Status {
+    CR_STATUS_ERROR = 0,
+    CR_STATUS_OK = 1,
+    CR_STATUS_SUSPENDED = 2
+};
+
+enum CR_Error {
+    CR_ERROR_NONE,
+    CR_ERROR_NO_MEMORY,
+    CR_ERROR_SYNTAX
+};
 CR_Parser CR_ParserCreate();
-void CR_PARSERFree(CR_Parser cp);
+void CR_ParserFree(CR_Parser cp);
+void CR_ParserReset(CR_Parser cp);
+
