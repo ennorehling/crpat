f3aceb5a945e433294c3f084bc22283107ca64a7
Enno Rehling <enno.rehling@gmail.com>
2018-07-26 12:52:45 +0200

    argument parsing fix

--- example/crprint.c
+++ example/crprint.c
@@ -28,7 +28,7 @@ int main(int argc, char **argv) {
     char buf[2048];
     int done = 0, err = 0;
 
-    if (argc > 0) {
+    if (argc > 1) {
         filename = argv[1];
         in = fopen(filename, "rt+");
         if (!in) {
